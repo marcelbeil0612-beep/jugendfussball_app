1542ed5cfc35ac9ec7108b8928ca1da9660d3204
marcelbeil0612-beep <marcelbeil0612@gmail.com>
2026-01-16 09:03:34 +0100

    feat(seed): add trainer team seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -5,12 +5,19 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function main() {
+  const teamName = "Jugendfussball";
   const email = "marcelbeil@gmx.de";
+
+  const team = await prisma.team.upsert({
+    where: { name: teamName },
+    update: {},
+    create: { name: teamName },
+  });
 
   await prisma.user.upsert({
     where: { email },
-    update: { role: "TRAINER" },
-    create: { email, role: "TRAINER" },
+    update: { role: "TRAINER", teamId: team.id },
+    create: { email, role: "TRAINER", teamId: team.id },
   });
 }
 
